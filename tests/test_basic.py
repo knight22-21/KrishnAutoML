5e69e38b90e3add782d405bc9f8789fbef09b6e9
knight22-21 <krishnatyagibest321@gmail.com>
2025-08-31 19:33:41 +0530

    MVP - 3 ci-cd added -11

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,7 +6,7 @@ def test_pipeline_runs(tmp_path):
     data = pd.DataFrame(
         {"feature1": list(range(10)), "feature2": ["a", "b"] * 5, "target": [0, 1] * 5}
     )
-    automl = KrishnAutoML(target="target")
+    automl = KrishnAutoML(target="target", n_splits=4)
     automl.load_data(data)
     automl.preprocess().train_models()
     metrics = automl.evaluate()
