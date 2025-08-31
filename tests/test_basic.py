d9a8b75231f2ce59dbc982c24551617414ba9ffa
knight22-21 <krishnatyagibest321@gmail.com>
2025-08-31 18:31:41 +0530

    MVP - 3 cli added

---
+++ tests/test_basic.py
@@ -0,0 +1,14 @@
+from krishnautoml import KrishnAutoML
+import pandas as pd
+
+def test_pipeline_runs(tmp_path):
+    data = pd.DataFrame({
+        "feature1": [1, 2, 3, 4, 5],
+        "feature2": ["a", "b", "a", "b", "a"],
+        "target": [0, 1, 0, 1, 0]
+    })
+    automl = KrishnAutoML(target="target")
+    automl.df = data
+    automl.preprocess().train_models()
+    metrics = automl.evaluate()
+    assert isinstance(metrics, dict)
